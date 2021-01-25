4050ea69663023cfde49925eb38484978159b556
Kit <lhk986@gmail.com>
2021-01-25 17:18:49 +0800

    adjustments to arcCam

--- scripts/model3d.js
+++ scripts/model3d.js
@@ -25,7 +25,8 @@ class Model3d extends HTMLElement{
                 // scene.clearColor = new BABYLON.Color3(1, 100, 1);
                 // scene.createDefaultCameraOrLight(true, true, true);
 
-                var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, BABYLON.Vector3.Zero(), scene);
+                // Parameters: name, alpha, beta, radius, target position, scene
+                var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, BABYLON.Vector3(0, 0, 0), scene);
                 var light = new BABYLON.DirectionalLight("DirectionalLight", new BABYLON.Vector3(0, -1, 0), scene);  
                 var frameRate = 10;
 
@@ -57,7 +58,8 @@ class Model3d extends HTMLElement{
 
                 rotate.setKeys(rotate_keys);
                 
-                camera.setPosition(new BABYLON.Vector3(15, 15, 0));
+                // Positions the camera overwriting alpha, beta, radius
+                camera.setPosition(new BABYLON.Vector3(0, 0, 30));
                 camera.attachControl(cnv, true);
 
                 scene.beginDirectAnimation(camera, rotate, 0, frameRate, true);
