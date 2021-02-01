51be9f09c69ee102352718ed971407874c8d7986
Kit <lhk986@gmail.com>
2021-02-01 15:16:08 +0800

    try to defaykt no anim

--- scripts/model3d_multipleTest.js
+++ scripts/model3d_multipleTest.js
@@ -149,6 +149,8 @@ class Model3d extends HTMLElement{
                 // speedRatio?: number, onAnimationEnd?: () => void, animatable?: Animatable, 
                 // stopCurrent?: boolean, targetMask?: (target: any) => boolean, onAnimationLoop?: () => void, isAdditive?: boolean
                 scene.beginAnimation(camera, 0, frameRate * 16, true);
+
+                animationGroups[0].stop();
                 
                 return scene;
             }
