493385bb652f62d1a76f85d5a4d43844afa081ce
Kit <lhk986@gmail.com>
2021-02-08 16:45:53 +0800

    colour try again

--- scripts/model3d_multipleTest.js
+++ scripts/model3d_multipleTest.js
@@ -318,7 +318,7 @@ class Model3d extends HTMLElement{
                     mesh.actionManager.registerAction(
                         new BABYLON.SetValueAction(BABYLON.ActionManager.OnPointerOutTrigger, mesh.material, "emissiveColor", mesh.material.emissiveColor));
                     mesh.actionManager.registerAction(
-                        new BABYLON.SetValueAction(BABYLON.ActionManager.OnPointerOverTrigger, mesh.material, "emissiveColor", BABYLON.Color4(128, 0, 0, 256)));
+                        new BABYLON.SetValueAction(BABYLON.ActionManager.OnPointerOverTrigger, mesh.material, "emissiveColor", BABYLON.Color3.Teal()));
 
                     // mesh.actionManager.registerAction(
                     //     new BABYLON.SetValueAction(BABYLON.ActionManager.OnPointerOutTrigger, mesh.material, "ambientColor", mesh.material.ambientColor));
