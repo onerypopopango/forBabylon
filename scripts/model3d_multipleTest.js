8b7e7abf63a4cc7dab4e1bc24a62ebe6e241e58f
Kit <lhk986@gmail.com>
2021-02-09 17:37:58 +0800

    name?

--- scripts/model3d_multipleTest.js
+++ scripts/model3d_multipleTest.js
@@ -315,7 +315,7 @@ class Model3d extends HTMLElement{
                 //  Over/Out <= this is it!!!! for hover flash anims  //
                 //****************************************************//
                 var makeOverOut = function (mesh) {
-                    var check = mesh.indexOf('building');
+                    var check = mesh.name.indexOf('building');
                     console.log('makeOverOut: ', mesh);
                     mesh.actionManager.registerAction(
                         new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPointerOverTrigger, function () {
