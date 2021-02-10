7a1233734ee5a2208bdfc67569ef8d2015d78889
Kit <lhk986@gmail.com>
2021-02-10 14:19:01 +0800

    light thing

--- scripts/model3d_multipleTest.js
+++ scripts/model3d_multipleTest.js
@@ -250,8 +250,8 @@ class Model3d extends HTMLElement{
 
             // lights in scene
             var light = new BABYLON.DirectionalLight("DirectionalLight1", new BABYLON.Vector3(4, -1, -2), scene);  
-            var lightSecond = new BABYLON.DirectionalLight("DirectionalLight2", new BABYLON.Vector3(2, 1, -3), scene);
-            var lightThird= new BABYLON.DirectionalLight("DirectionalLight3", new BABYLON.Vector3(-2, 1, 3), scene);
+            var lightSecond = new BABYLON.DirectionalLight("DirectionalLight2", new BABYLON.Vector3(2, -1, -3), scene);
+            var lightThird= new BABYLON.DirectionalLight("DirectionalLight3", new BABYLON.Vector3(-2, -1, 3), scene);
 
             // create lightmap texture & shadow generator
             lightmap = new BABYLON.Texture("../textures/Lightmap-0_comp_light.exr", scene);
