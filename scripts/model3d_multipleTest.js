6771b78611e900b40247c8a5ea93f173f78dc2b4
Kit <lhk986@gmail.com>
2021-02-10 15:04:34 +0800

    ????

--- scripts/model3d_multipleTest.js
+++ scripts/model3d_multipleTest.js
@@ -252,7 +252,7 @@ class Model3d extends HTMLElement{
 
             // lights in scene
             var light = new BABYLON.DirectionalLight("DirectionalLight1", new BABYLON.Vector3(4, -1, -2), scene);  
-            var lightSecond = new BABYLON.DirectionalLight("DirectionalLight2", new BABYLON.Vector3(2, -1, -3), scene);
+            var lightSecond = new BABYLON.DirectionalLight("DirectionalLight2", new BABYLON.Vector3(2, -1, 3), scene);
             var lightThird= new BABYLON.DirectionalLight("DirectionalLight3", new BABYLON.Vector3(-2, -1, 3), scene);
 
             light.specular = new BABYLON.Color3(0, 0, 0);
