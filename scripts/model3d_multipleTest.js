7f9dc18c9a55e0195f4c4833b35f6b91a1b5bc6d
Kit <lhk986@gmail.com>
2021-02-15 17:09:13 +0800

    try again

--- scripts/model3d_multipleTest.js
+++ scripts/model3d_multipleTest.js
@@ -149,7 +149,7 @@ class Model3d extends HTMLElement{
                 rotate.setKeys(rotate_keys);
                 
                 // Positions the camera overwriting alpha, beta, radius
-                camera.setPosition(new BABYLON.Vector3(0, 30, 120));
+                // camera.setPosition(new BABYLON.Vector3(0, 30, 120));
                 // camera.attachControl(cnv, true);
 
                 //so beginDirectAnimation didn't work... this did instead
