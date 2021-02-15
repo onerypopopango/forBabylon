082726de2861c7d42ccf897235b2d531fdefa551
Kit <lhk986@gmail.com>
2021-02-15 16:38:29 +0800

    try skybox again sigh

--- scripts/model3d_multipleTest.js
+++ scripts/model3d_multipleTest.js
@@ -87,7 +87,7 @@ class Model3d extends HTMLElement{
                 var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size:1000.0}, scene);
                 var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
                 skyboxMaterial.backFaceCulling = false;
-                skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("../textures/skybox", scene, ".jpg");
+                skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("../textures/skybox", scene);
                 skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
                 skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
                 skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
