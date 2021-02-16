86d3c03d4dd797e62bcf5d090a6fc96bf8061c9b
Kit <lhk986@gmail.com>
2021-02-16 12:32:41 +0800

    cleaning code

--- scripts/model3d_multipleTest.js
+++ scripts/model3d_multipleTest.js
@@ -338,20 +338,6 @@ class Model3d extends HTMLElement{
                             }
                         })
                     );
-
-                    // mesh.actionManager.registerAction(
-                    //     new BABYLON.SetValueAction(
-                    //         BABYLON.ActionManager.OnPointerOutTrigger, mesh.material, "emissiveColor", mesh.material.emissiveColor));
-                    // mesh.actionManager.registerAction(
-                    //     new BABYLON.SetValueAction(
-                    //         BABYLON.ActionManager.OnPointerOverTrigger, mesh.material, "emissiveColor", BABYLON.Color4.FromColor3(BABYLON.Color3.Teal(), 1)));
-
-                    // mesh.actionManager.registerAction(
-                    //     new BABYLON.SetValueAction(BABYLON.ActionManager.OnPointerOutTrigger, mesh.material, "ambientColor", mesh.material.ambientColor));
-                    // mesh.actionManager.registerAction(
-                    //     new BABYLON.SetValueAction(BABYLON.ActionManager.OnPointerOverTrigger, mesh.material, "ambientColor", BABYLON.Color3.White()));
-                    // mesh.actionManager.registerAction(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPointerOutTrigger, mesh, "scaling", new BABYLON.Vector3(1, 1, 1), 150));
-                    // mesh.actionManager.registerAction(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPointerOverTrigger, mesh, "scaling", new BABYLON.Vector3(1.1, 1.1, 1.1), 150));
                 }
 
                 var meshClick = function (mesh) {
