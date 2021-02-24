9ddb5ab7d8cc8476dce8c1bdc65ad076bb601460
Kit <lhk986@gmail.com>
2021-02-24 17:17:04 +0800

    light again

--- scripts/model3d_floorplanAnim.js
+++ scripts/model3d_floorplanAnim.js
@@ -139,13 +139,15 @@ class Model3d extends HTMLElement{
             var light = new BABYLON.DirectionalLight("DirectionalLight1", new BABYLON.Vector3(4, -1, 2), scene);  
             var lightSecond = new BABYLON.DirectionalLight("DirectionalLight2", new BABYLON.Vector3(2, -1, 3), scene);
             var lightThird= new BABYLON.DirectionalLight("DirectionalLight3", new BABYLON.Vector3(-2, -1, -3), scene);
-            var lightFourth= new BABYLON.DirectionalLight("DirectionalLight3", new BABYLON.Vector3(-1, -1, -3), scene);
 
             light.intensity = 2;
             light.specular = new BABYLON.Color3(0, 0, 0);
             lightSecond.specular = new BABYLON.Color3(0, 0, 0);
             lightThird.specular = new BABYLON.Color3(0, 0, 0);
-            lightFourth.specular = new BABYLON.Color3(0, 0, 0);
+
+            // this adds a lot of light
+            var hemiLight = new BABYLON.HemisphericLight("hemiLight", new BABYLON.Vector3(-1, 1, 0), scene);
+            hemiLight.specular = new BABYLON.Color3(0, 0, 0);
 
             floorPlanMaterial = new BABYLON.Texture("../textures/Floorplan_2_Base.png", scene);
 
