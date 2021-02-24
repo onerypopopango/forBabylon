adae27bb5e84b79dc6e1ee93ff4233cd7b6b1085
Kit <lhk986@gmail.com>
2021-02-24 11:40:01 +0800

    test

--- scripts/model3d_floorplanAnim.js
+++ scripts/model3d_floorplanAnim.js
@@ -489,7 +489,7 @@ class Model3d extends HTMLElement{
 
                     tiltUp_keys.push({
                         frame: frameRate*16,
-                        value: 60
+                        value: -60
                     });
 
                     tiltUp.setKeys(tiltUp_keys);
