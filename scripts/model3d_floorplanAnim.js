2bd35d84efaeb28a22ba032b2340556f2d84e3b8
Kit <lhk986@gmail.com>
2021-03-03 18:39:11 +0800

    asdjhaskjd

--- scripts/model3d_floorplanAnim.js
+++ scripts/model3d_floorplanAnim.js
@@ -296,7 +296,7 @@ class Model3d extends HTMLElement{
                 var cctvName =  new BABYLON.GUI.TextBlock();
                 cctvName.text = "CCTV 1 - ALERT";
                 cctvName.color =  "red";
-                cctvName.linkOffsetY = 120;
+                cctvName.linkOffsetY = 122;
 
                 // gui line
                 var line = new BABYLON.GUI.MultiLine();
