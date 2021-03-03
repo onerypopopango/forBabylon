2b04411bfa0e671b095a28da8fc3ec3b41d4f97c
Kit <lhk986@gmail.com>
2021-03-03 13:45:27 +0800

    asdasdasdad

--- scripts/model3d_floorplanAnim.js
+++ scripts/model3d_floorplanAnim.js
@@ -297,7 +297,7 @@ class Model3d extends HTMLElement{
                 line.lineWidth = 4;
                 line.color = "Red";
                 line.scaleY = 0.7;
-                line.transformCenterY = -2;
+                line.transformCenterY = 1;
 
                 //****************************************************//
                 //  Over/Out <= this is it!!!! for hover flash anims  //
